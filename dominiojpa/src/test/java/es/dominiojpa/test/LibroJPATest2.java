27d15563daf2d1b7bf0b267eeaffc0ca35bc4a14
Razvan Pascal <razvanpascal@outlook.com>
2020-04-03 13:51:36 +0200

    junit actualizacion

--- dominiojpa/src/test/java/es/dominiojpa/test/LibroJPATest2.java
+++ dominiojpa/src/test/java/es/dominiojpa/test/LibroJPATest2.java
@@ -1,45 +1,72 @@
 package es.dominiojpa.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 import es.avalon.dominiojpa.Libro;
 
 public class LibroJPATest2 {
 	
-	EntityManagerFactory emf;
+	static EntityManagerFactory emf;
 	EntityManager em;
 	
-	@Test
-	public void testInicialLibros() {
-		
+	@BeforeClass
+	public static void setUpClass() {
 		Persistence.generateSchema("UnidadBiblioteca", null);
-		
+		//es general para toda la aplicacion
 		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+	}
+	
+	
+	@Before
+	public void setUp() {
+		//existe por cada operacion que hacemos con un grupo de entidades
 		em = emf.createEntityManager();
+		
+	}
+	
+	@Test
+	public void testListaTodosLosLibros() {
+		
 		//Consulta JPA Query Language
 		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
 		
 		List<Libro> lista = consulta.getResultList();
-		assertEquals(4, lista.size());
+		
+		//assertEquals(4, lista.size());
+		//assertNotNull(lista);
+		//assertTrue(lista.size()>=4);
+		
+		assertThat(lista.size(), greaterThanOrEqualTo(4));
 		
 	}
+
+
+
 	@Test
 	public void testExisteLibroTituloJava() {
 		
-		Persistence.generateSchema("UnidadBiblioteca", null);
 		
-		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
-		em = emf.createEntityManager();
 		//Consulta JPA Query Language
 		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
 		
@@ -52,18 +79,15 @@ public class LibroJPATest2 {
 		 * que en la lista viene con el metodo contains
 		 */
 		Libro libro = new Libro("1AB");
-		assertTrue(lista.contains(libro));
+		
+		//assertTrue(lista.contains(libro));
+		assertThat(lista,hasItems(libro));
 		
 	}
 	
 	@Test
-	public void testBuscarLibroPorISBN() {
-		
-		Persistence.generateSchema("UnidadBiblioteca", null);
-		
-		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
-		em = emf.createEntityManager();
-		
+	public void testBuscarLibroPorISBN1AB() {
+			
 		Libro libro = em.find(Libro.class, "1AB");
 		
 		assertEquals("1AB", libro.getIsbn());
@@ -72,7 +96,69 @@ public class LibroJPATest2 {
 		assertEquals(10, libro.getPrecio());
 		assertEquals("java", libro.getCategoria());
 		
+	}
+	
+	@Test
+	public void testBuscarLibroPorAutorCecilio() {
+		
+		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l where l.autor=:autor", Libro.class);
+		consulta.setParameter("autor", "cecilio");
+		
+		//Me devuelve la lista de libros
+		List<Libro> lista = consulta.getResultList();
+		
+		assertEquals(2, lista.size());
+	}
+	
+	
+	@Test
+	public void testBorrarLibroPorISBN1AB() {
+		
+		// busca la entidad
+		Libro libro = em.find(Libro.class, "1AB");
+		// hace un assert y comprueba que existe
+		assertNotNull(libro);
+		
+		//genera una transaccion
+		EntityTransaction t = em.getTransaction();
+		//ejecuta la transaccion
+		t.begin();
+		// elimina el libro
+		em.remove(libro);
+		
+		//intenta encontrar el libro
+		Libro libro2=em.find(Libro.class, "1AB");
+		
+		//comprueba que no existe
+		assertNull(libro2);
+		
+		// echa la transaccion para atrás de forma que los datos queden como
+		// estaban en la base de datos
+		t.rollback();
+		
 		
 	}
+	
+	@Test
+	public void testBuscarLibroPorCategoriaWeb() {
+		
+		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l where l.categoria=:categoria", Libro.class);
+		consulta.setParameter("categoria", "web");
+		
+		//Me devuelve la lista de libros
+		List<Libro> lista = consulta.getResultList();
+		
+		assertEquals(2, lista.size());
+	}
+	
+	@After
+	public void close() {
+		//emf.close();
+		em.close();
+		//emf = null;
+		em = null;
+		
+	}
+	
 
 }
--- dominiojpa/src/test/resources/META-INF/insertar.sql
+++ dominiojpa/src/test/resources/META-INF/insertar.sql
@@ -2,3 +2,4 @@ INSERT INTO `Libros` (`isbn`, `titulo`, `autor`, `precio`, `categoria`) VALUES (
 INSERT INTO `Libros` (`isbn`, `titulo`, `autor`, `precio`, `categoria`) VALUES ('2AC', 'Java Web', 'cecilio', 15, 'java')
 INSERT INTO `Libros` (`isbn`, `titulo`, `autor`, `precio`, `categoria`) VALUES ('3BC', 'html', 'gema', 8, 'web')
 INSERT INTO `Libros` (`isbn`, `titulo`, `autor`, `precio`, `categoria`) VALUES ('4BD', 'html5', 'andres', 20, 'web')
+INSERT INTO `Libros` (`isbn`, `titulo`, `autor`, `precio`, `categoria`) VALUES ('5EF', 'python', 'andres', 20, 'programacion')
