3c4882fece72d58ad1be340ec55d7a49c07ddce3
cursos <cursos@localhost>
2020-04-03 10:29:15 +0200

    primeros Test

---
+++ dominiojpa/src/test/java/es/dominiojpa/test/LibroJPATest2.java
@@ -0,0 +1,76 @@
+package es.dominiojpa.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.junit.Test;
+
+import es.avalon.dominiojpa.Libro;
+
+public class LibroJPATest2 {
+
+	EntityManagerFactory emf;
+	EntityManager em; 
+	
+	
+	@Test
+	public void testInicialLibros() {
+		
+		Persistence.generateSchema("UnidadBiblioteca", null);
+		
+		emf=Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		em= emf.createEntityManager();
+		//consulta JPA Query Language
+		TypedQuery<Libro> consulta=em.createQuery("select l from Libro l",Libro.class);
+		
+		List<Libro> lista= consulta.getResultList();
+		assertEquals(4,lista.size());
+		
+		
+	}
+	
+	@Test
+	public void test_Existe_Libro_Titulo_Java() {
+		
+		Persistence.generateSchema("UnidadBiblioteca", null);
+		
+		emf=Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		em= emf.createEntityManager();
+		//consulta JPA Query Language
+		TypedQuery<Libro> consulta=em.createQuery("select l from Libro l",Libro.class);
+		
+		// me devuelve la lista de libros
+		List<Libro> lista= consulta.getResultList();
+		// genero en memoria el libro 1AB
+		// como tenemos sobrecargado el equals y el hashcode
+		// instancio el libro y comprueba 
+		// que en la lista viene con el metodo contains
+		Libro libro= new Libro("1AB");
+		assertTrue(lista.contains(libro));
+		
+		
+		
+	}
+	
+	@Test
+	public void test_buscar_libro_por_isbn() {
+		
+		Persistence.generateSchema("UnidadBiblioteca", null);
+		
+		emf=Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		em= emf.createEntityManager();
+		Libro libro= em.find(Libro.class, "1AB");
+		
+		
+		
+	}
+	
+
+}
